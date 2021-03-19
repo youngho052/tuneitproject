c82c8e8dd0a1d870f2d52ab7c562983e58c9c051
youngho052 <qkrdudgh2701@gmail.com>
2021-03-19 13:28:59 +0900

    nav layout

--- src/Pages/Components/NavBar/Nav.jsx
+++ src/Pages/Components/NavBar/Nav.jsx
@@ -1,52 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { MENU_CONT } from "./Data/Menu";
 import { LANGUAGE } from "./Data/Language";
 
 function Nav(props) {
+  const [toggle, setToggle] = useState(true);
+
+  const clickHandle = () => {
+    setToggle(!toggle);
+  };
+
+  console.log(toggle);
   return (
     <NavContainer>
-      {/* <div className="inner"> */}
-      <h1>
+      <H1>
         <span className="blind">tuneit</span>
-
-        <span className="blind">tuneit</span>
-      </h1>
-      <button type="button" className="menu-open for-mobile">
-        <svg
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 40 40"
-        >
-          <g id="Menu" transform="translate(-305 -94)">
-            <path
-              id="White"
-              className="svg-menu"
-              d="M315,122v-2h20v2H315z M315,115v-2h20v2H315z M315,108v-2h20v2H315z"
-            />
-          </g>
-        </svg>
-      </button>
-      <div id="topMenu">
-        <MenuBar>
-          {/* <div className="gnb-wrap"> */}
-          <ul className="gnb" id="menu">
-            {MENU_CONT.map((item, index) => {
-              return <li>{item.content}</li>;
-            })}
-          </ul>
-          {/* </div> */}
-        </MenuBar>
-        <div className="language-area" id="language">
-          <ul>
-            {LANGUAGE.map((item, index) => {
-              return <li>{item.language}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
+      </H1>
+      {/* <div id="topMenu"> */}
+      <MenuBar>
+        {/* <div className="gnb-wrap"> */}
+        <ul className="gnb" id="menu">
+          {MENU_CONT.map((item, index) => {
+            return <li key={index}>{item.content}</li>;
+          })}
+        </ul>
+        {/* </div> */}
+      </MenuBar>
+      {/* <div className="language-area" id="language"> */}
+      <LanguageBar>
+        <ul>
+          {LANGUAGE.map((item, index) => {
+            return (
+              <li toggle={toggle} key={index} onClick={clickHandle}>
+                {item.language}
+              </li>
+            );
+          })}
+        </ul>
+      </LanguageBar>
+      {/* </div> */}
       {/* </div> */}
     </NavContainer>
   );
@@ -55,21 +47,44 @@ function Nav(props) {
 export default Nav;
 
 const NavContainer = styled.div`
-  position: relative;
-  height: 100%;
-  text-align: center;
-  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  height: 100vh;
+  padding: 50px;
+  background-color: #252525;
+`;
+
+const H1 = styled.h1`
+  color: #fff;
+  font-size: 30px;
 `;
 
 const MenuBar = styled.div`
-  position: absolute;
-  left: 50%;
-  -webkit-transform: translateX(-50%);
-  -ms-transform: translateX(-50%);
-  -o-transform: translateX(-50%);
-  transform: translateX(-50%);
-
-  .gnb {
+  ul {
     display: flex;
+    justify-content: space-between;
+    width: 480px;
+
+    li {
+      color: #999999;
+      cursor: pointer;
+
+      &:hover {
+        color: #f36d1a;
+      }
+    }
+  }
+`;
+
+const LanguageBar = styled.div`
+  ul {
+    display: flex;
+    justify-content: space-between;
+    width: 60px;
+
+    li {
+      color: ${(props) => (props.toggle === true ? "#fff" : "#999999")};
+      cursor: pointer;
+    }
   }
 `;
